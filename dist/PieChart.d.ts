de67932471669e6b25d547915fb8665b22d9e365
ORLIN BENOV <orlin.benov@gmail.com>
2023-03-08 11:20:40 +0200

    initial files

---
+++ dist/PieChart.d.ts
@@ -0,0 +1,23 @@
+/// <reference types="react" />
+import { ViewStyle } from "react-native";
+import AbstractChart, { AbstractChartProps } from "./AbstractChart";
+export interface PieChartProps extends AbstractChartProps {
+    data: Array<any>;
+    width: number;
+    height: number;
+    accessor: string;
+    backgroundColor: string;
+    paddingLeft: string;
+    center?: Array<number>;
+    absolute?: boolean;
+    hasLegend?: boolean;
+    hasChart?: boolean;
+    style?: Partial<ViewStyle>;
+    avoidFalseZero?: boolean;
+}
+declare type PieChartState = {};
+declare class PieChart extends AbstractChart<PieChartProps, PieChartState> {
+    render(): JSX.Element;
+}
+export default PieChart;
+//# sourceMappingURL=PieChart.d.ts.map
